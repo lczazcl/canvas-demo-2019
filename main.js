fca7bec40420fc937e00e8632e9595eeb5c007e2
lcz <1193368261@qq.com>
2019-01-21 14:42:02 +0800

    新建了三个文件

---
+++ main.js
@@ -0,0 +1,100 @@
+var yyy = document.getElementById('xxx')
+var context = yyy.getContext('2d')
+
+autoSetCanvasSize(yyy)
+
+/*************************/
+listenToMouse(yyy)
+
+/************************************/
+var eraserEnabled = false
+eraser.onclick = function () {
+  eraserEnabled = true
+  actions.className = 'actions x'
+}
+
+brush.onclick = function () {
+  eraserEnabled = false
+  actions.className = 'actions'
+}
+
+/***********************************************/
+function autoSetCanvasSize(canvas) {
+  setCanvasSize()
+
+  window.onresize = function () {
+    setCanvasSize()
+  }
+
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+
+    canvas.width = pageWidth
+    canvas.height = pageHeight
+  }
+}
+
+function drawCircle(x, y, radius) {
+  context.beginPath()
+  context.fillStyle = 'black'
+  context.arc(x, y, radius, 0, Math.PI * 2)
+  context.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath()
+  context.strokeStyle = 'black'
+  context.moveTo(x1, y1) //起点
+  context.lineWidth = 5
+  context.lineTo(x2, y2) //终点
+  context.stroke()
+  context.closePath()
+}
+
+function listenToMouse(canvas) {
+  var using = false //是否为橡皮状态
+
+  var lastPoint = {
+    x: undefined,
+    y: undefined
+  }
+
+  canvas.onmousedown = function (ev) {
+    var x = ev.clientX
+    var y = ev.clientY
+    using = true
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      // 鼠标按下时记录最后一个点
+      lastPoint = {
+        'x': x,
+        'y': y
+      }
+    }
+  }
+
+  canvas.onmousemove = function (ev) {
+    var x = ev.clientX
+    var y = ev.clientY
+
+    if (!using) { return }
+
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      var newPoint = {
+        'x': x,
+        'y': y
+      }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      // 鼠标移动之后最新的点为一开始记录的点
+      lastPoint = newPoint
+    }
+  }
+
+  canvas.onmouseup = function (ev) {
+    using = false
+  }
+}
