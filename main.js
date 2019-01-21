712499b6de45498c739664976e39f9d1fd04c23e
lcz <1193368261@qq.com>
2019-01-21 21:49:59 +0800

    添加下载

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var yyy = document.getElementById('xxx')
 var context = yyy.getContext('2d')
+var lineWidth = 5
 
 autoSetCanvasSize(yyy)
 
@@ -8,40 +9,75 @@ listenToUser(yyy)
 
 /************************************/
 var eraserEnabled = false
-pen.onclick = function (){
+pen.onclick = function () {
   eraserEnabled = false
   pen.classList.add('active')
   eraser.classList.remove('active')
 }
 
-eraser.onclick = function (){
+eraser.onclick = function () {
   eraserEnabled = true
   eraser.classList.add('active')
   pen.classList.remove('active')
 }
 
-red.onclick = function (){
-  context.fillStyle = 'red'
-  context.strokeStyle = 'red'
-  red.classList.add('active')
+black.onclick = function () {
+  context.fillStyle = 'black'
+  context.strokeStyle = 'black'
+  black.classList.add('active')
+  red.classList.remove('active')
   green.classList.remove('active')
   blue.classList.remove('active')
 }
 
-green.onclick = function (){
+red.onclick = function () {
+  context.fillStyle = 'red'
+  context.strokeStyle = 'red'
+  red.classList.add('active')
+  black.classList.remove('active')
+  green.classList.remove('active')
+  blue.classList.remove('active')
+}
+
+green.onclick = function () {
   context.fillStyle = 'green'
   context.strokeStyle = 'green'
   green.classList.add('active')
   red.classList.remove('active')
+  black.classList.remove('active')
   blue.classList.remove('active')
 }
 
-blue.onclick = function (){
+blue.onclick = function () {
   context.fillStyle = 'blue'
   context.strokeStyle = 'blue'
   blue.classList.add('active')
   red.classList.remove('active')
+  black.classList.remove('active')
   green.classList.remove('active')
+}
+
+clear.onclick = function () {
+  context.clearRect(0, 0, yyy.width, yyy.height)
+}
+
+download.onclick = function () {
+  var url = yyy.toDataURL("image/png")
+  console.log(url)
+  var a = document.createElement('a')
+  document.body.appendChild(a)
+  a.href = url
+  a.download = '我的画布'
+  a.target = '_blank'
+  a.click()
+}
+
+thin.onclick = function () {
+  lineWidth = 5
+}
+
+thick.onclick = function () {
+  lineWidth = 10
 }
 
 /***********************************************/
@@ -72,7 +108,7 @@ function drawLine(x1, y1, x2, y2) {
   context.beginPath()
   //context.strokeStyle = 'black'
   context.moveTo(x1, y1) //起点
-  context.lineWidth = 5
+  context.lineWidth = lineWidth
   context.lineTo(x2, y2) //终点
   context.stroke()
   context.closePath()
